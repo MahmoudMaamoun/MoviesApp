b008b1ba55156f16e44d1b3aad7b45098133de29
Mahmoud Maamoun <maamounsharaky@gmail.com>
2024-08-28 02:27:47 +0300

    Initial Commit

---
+++ banquemisr.challenge05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  banquemisr.challenge05
+//
+//  Created by Mahmoud Maamoun on 28/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
